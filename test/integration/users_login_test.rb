75fd3be58d4435dd6ea28daf57177895f364fd06
Asher Dale <asher@dales.org>
2017-09-28 15:18:53 +0300

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -27,6 +27,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', edit_user_path(@user)
 
     delete logout_path
     assert_not is_logged_in?
