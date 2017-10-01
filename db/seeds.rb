a9dd67beca5ea4067698bef28fc72af620920c09
Asher Dale <asher@dales.org>
2017-10-01 14:32:39 +0300

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,17 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+User.create!(name:  "Asher Dale",
+             email: "asor1999@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
@@ -11,6 +21,8 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 
 end
