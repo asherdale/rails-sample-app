2e8ec05512ab8bea9be5f5680399533e1895dd6f
Asher Dale <asher@dales.org>
2017-10-09 13:24:15 +0300

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,5 +24,10 @@ User.create!(name:  "Asher Dale",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
 
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
